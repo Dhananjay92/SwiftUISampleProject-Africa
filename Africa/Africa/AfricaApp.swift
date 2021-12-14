082f71fbbba1ded4ce930400818bea3a775b3e9d
Mehmet Karanlık <89414084+MehmetKaranlik@users.noreply.github.com>
2021-12-14 19:49:46 +0300

    First commit [Project Start]

---
+++ Africa/Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by mehmet karanlık on 14.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
